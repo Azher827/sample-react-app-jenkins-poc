2499e4fff3fc1178e3a7f9b7f955142a72b7e0cc
Azher827 <84763263+Azher827@users.noreply.github.com>
2021-09-06 17:04:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This App is Deployed Using Jenkins & Argo CD Testing
         </p>
       </div>
     );
